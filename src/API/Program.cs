6d50d1c941474beb242660df3b47b32b5370b061
John Criste <54412955+johnl28@users.noreply.github.com>
2024-12-08 23:01:26 +0000

    Add interim CORS

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 
 var builder = WebApplication.CreateBuilder(args);
-
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -19,6 +19,16 @@ builder.Services.AddDbContext<ContactContext>(options =>
 
 builder.Services.AddScoped<IContactRepository, ContactRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://localhost:5175");
+                          policy.AllowAnyHeader();
+                      });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,6 +38,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(MyAllowSpecificOrigins);
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
