30f263b68c9c8c457ce083b003558ea1d06ed4b3
John <ionutcriste28@gmail.com>
2025-02-18 18:58:06 +0000

    Add getContact by id method

--- src/VueApp/src/services/contactsService.ts
+++ src/VueApp/src/services/contactsService.ts
@@ -3,6 +3,11 @@ import { axiosInstance } from "./axiosInstance";
 
 export default {
 
+  async getContact(id: number): Promise<AxiosResponse>
+  {
+    return await axiosInstance.get(`contact/${id}`);
+  },
+
   async getContacts(): Promise<AxiosResponse<Contact[]>>
   {
     return await axiosInstance.get<Contact[]>("contact");
